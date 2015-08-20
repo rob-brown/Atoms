7b8d8b5ada4d5831ba2e07f6e962227f8b78e0ff
Robert Brown <ammoknight@gmail.com>
2015-08-20 07:45:31 -0600

    Added Atom ASCII header.

--- Atoms/FiltersViewController.swift
+++ Atoms/FiltersViewController.swift
@@ -1,10 +1,36 @@
-//
-//  FiltersViewController.swift
-//  Atoms
-//
-//  Created by Robert Brown on 8/6/15.
-//  Copyright © 2015 Robert Brown. All rights reserved.
-//
+//               ~MMMMMMMM,.           .,MMMMMMM ..
+//              DNMMDMMMMMMMM,.     ..MMMMNMMMNMMM,
+//              MMM..   ...NMMM    MMNMM       ,MMM
+//             NMM,        , MMND MMMM          .MM
+//             MMN             MMMMM             MMM
+//            .MM           , MMMMMM ,           MMM
+//            .MM            MMM. MMMM           MMM
+//            .MM~         .MMM.   NMMN.         MMM
+//             MMM        MMMM: .M ..MMM        .MM,
+//             MMM.NNMMMMMMMMMMMMMMMMMMMMMMMMMM:MMM,
+//         ,,MMMMMMMMMMMMMMM           NMMDNMMMMMMMMN~,
+//        MMMMMMMMM,,  OMMM             NMM  . ,MMNMMMMN.
+//     ,MMMND  .,MM=  NMM~    MMMMMM+.   MMM.  NMM. .:MMMMM.
+//    MMMM       NMM,MMMD   MMM$ZZZMMM:  .NMN.MMM        NMMM
+//  MMNM          MMMMMM   MMZO~:ZZZZMM~   MMNMMN         .MMM
+//  MMM           MMMMM   MMNZ~~:ZZZZZNM,   MMMM            MMN.
+//  MM.           .MMM.   MMZZOZZZZZZZMM.   MMMM            MMM.
+//  MMN           MMMMN   MMMZZZZZZZZZNM.   MMMM            MMM.
+//  NMMM         .MMMNMN  .MM$ZZZZZZZMMN ..NMMMMM          MMM
+//   MMMMM       MMM.MMM~  .MNMZZZZMMMD   MMM MMM .    . NMMN,
+//     NMMMM:  ..MM8  MMM,  . MNMMMM:   .MMM:  NMM  ..MMMMM
+//     ...MMMMMMNMM    MMM      ..      MMM.    MNDMMMMM.
+//        .: MMMMMMMMMMDMMND           MMMMMMMMNMMMMM
+//             NMM8MNNMMMMMMMMMMMMMMMMMMMMMMMMMMNMM
+//            ,MMM        NMMMDMMMMM NMM.,.     ,MM
+//             MMO        ..MMM    NMMM          MMD
+//            .MM.         ,,MMM+.MMMM=         ,MMM
+//            .MM.            MMMMMM~.           MMM
+//             MM=             MMMMM..          .MMN
+//             MMM           MMM8 MMMN.          MM,
+//             +MMO        MMMN,   MMMMM,       MMM
+//             ,MMMMMMM8MMMMM,      . MMNMMMMMMMMM.
+//               .NMMMMNMM              DMDMMMMMM
 
 import UIKit
 
